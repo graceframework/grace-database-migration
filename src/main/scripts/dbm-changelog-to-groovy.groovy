945183485a5649d851dde7dfbf471bc9036de9c4
Michael Yan <rain@rainboyan.com>
2024-01-10 20:59:24 +0800

    Grace: migrate to grace

--- src/main/scripts/dbm-changelog-to-groovy.groovy
+++ src/main/scripts/dbm-changelog-to-groovy.groovy
@@ -2,7 +2,7 @@ import org.grails.plugins.databasemigration.DatabaseMigrationException
 import org.grails.plugins.databasemigration.command.DbmChangelogToGroovy
 
 description('Converts a changelog file to a Groovy DSL file') {
-    usage 'grails [environment] dbm-changelog-to-groovy [src_file_name] [dest_file_name]'
+    usage 'grace [environment] dbm-changelog-to-groovy [src_file_name] [dest_file_name]'
     flag name: 'src_file_name', description: 'The name and path of the changelog file to convert'
     flag name: 'dest_file_name', description: 'The name and path of the Groovy file'
     flag name: 'dataSource', description: 'if provided will run the script for the specified dataSource creating a file named changelog-dataSource.groovy if a filename is not given. Not needed for the default dataSource'
--- src/main/scripts/dbm-create-changelog.groovy
+++ src/main/scripts/dbm-create-changelog.groovy
@@ -2,7 +2,7 @@ import org.grails.plugins.databasemigration.DatabaseMigrationException
 import org.grails.plugins.databasemigration.command.DbmCreateChangelog
 
 description('Creates an empty changelog file') {
-    usage 'grails [environment] dbm-create-changelog [filename]'
+    usage 'grace [environment] dbm-create-changelog [filename]'
     flag name: 'filename', description: 'The path to the output file to write to'
     flag name: 'dataSource', description: 'if provided will run the script for the specified dataSource creating a file named changelog-dataSource.groovy if a filename is not given. Not needed for the default dataSource'
     flag name: 'force', description: 'Whether to overwrite existing files'
