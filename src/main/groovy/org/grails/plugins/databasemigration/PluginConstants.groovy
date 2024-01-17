840997e3698f577051032bae588f2f63e6710e75
Michael Yan <rain@rainboyan.com>
2024-01-17 18:26:43 +0800

    Change default changelog location to db/migrations

--- src/main/groovy/org/grails/plugins/databasemigration/PluginConstants.groovy
+++ src/main/groovy/org/grails/plugins/databasemigration/PluginConstants.groovy
@@ -3,5 +3,5 @@ package org.grails.plugins.databasemigration
 class PluginConstants {
     static final String DATA_SOURCE_NAME_KEY = "dataSourceName"
     static final String DEFAULT_DATASOURCE_NAME = 'dataSource'
-    static final String DEFAULT_CHANGE_LOG_LOCATION = 'app/migrations'
+    static final String DEFAULT_CHANGE_LOG_LOCATION = 'db/migrations'
 }
